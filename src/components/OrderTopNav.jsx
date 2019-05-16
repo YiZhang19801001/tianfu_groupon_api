b86e6761e59394c6baceced99b991c83bfa6b9e8
yizhang199 <yizhang19801001@gmail.com>
2019-05-17 09:43:58 +1000

    order mangement tabs reorder

--- src/components/OrderTopNav.jsx
+++ src/components/OrderTopNav.jsx
@@ -16,19 +16,20 @@ const OrderTopNav = ({ location }) => {
         className={getClass("/orders/products")}
         to={`${process.env.PUBLIC_URL}/orders/products`}
       >
-        产品团购进度
+        产品生产报表
+      </Link>
+
+      <Link
+        className={getClass("/orders/stores")}
+        to={`${process.env.PUBLIC_URL}/orders/stores`}
+      >
+        产品配送报表
       </Link>
       <Link
         className={getClass("/orders/customers")}
         to={`${process.env.PUBLIC_URL}/orders/customers`}
       >
-        用户团购订单
-      </Link>
-      <Link
-        className={getClass("/orders/stores")}
-        to={`${process.env.PUBLIC_URL}/orders/stores`}
-      >
-        店面订单管理
+        用户提货报表
       </Link>
     </div>
   );
