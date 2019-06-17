e57e2933e307c0bcd06178d93f74cb7f32165383
yizhang199 <yizhang19801001@gmail.com>
2019-06-17 12:48:14 +1000

    debug: orderNav

--- src/components/OrderTopNav.jsx
+++ src/components/OrderTopNav.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const OrderTopNav = ({ location }) => {
+  const path = location.pathname;
   const getClass = value => {
-    const path = location.pathname;
     if (value === path) {
       return "active";
     }
@@ -13,23 +13,25 @@ const OrderTopNav = ({ location }) => {
   return (
     <div className="component-order-top-nav">
       <Link
-        className={getClass("/orders/products")}
+        className={
+          path === "/orders/products" || path === "/orders" ? "active" : ""
+        }
         to={`${process.env.PUBLIC_URL}/orders/products`}
       >
-        产品生产报表
+        产品统计
       </Link>
 
       <Link
         className={getClass("/orders/stores")}
         to={`${process.env.PUBLIC_URL}/orders/stores`}
       >
-        产品配送报表
+        取货时间地点统计
       </Link>
       <Link
         className={getClass("/orders/customers")}
         to={`${process.env.PUBLIC_URL}/orders/customers`}
       >
-        用户提货报表
+        订单统计
       </Link>
     </div>
   );
