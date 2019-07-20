b61e4af31b582fff3bdf130f95b908ed9a3a6fa2
roben <yizhang19801001@gmail.com>
2019-07-20 21:15:15 +1000

    products worksheets

--- src/components/orders/products/Section.jsx
+++ src/components/orders/products/Section.jsx
@@ -10,43 +10,49 @@ export default ({ orderProductArray, index }) => {
   return (
     <div
       className="section"
-      key={`orderProductsArray${orderProductArray[0].location_id}`}
     >
       {/* <TableView productsList={orderProductArray} /> */}
       <div className={`header`} onClick={toggleTable}>
         <p>
-          {orderProductArray[0].store_name}:{" "}
-          {`共 ${getTotal(orderProductArray)} 个菜`}
+          {orderProductArray.location}
         </p>
-        <div
-          className={`control`}
-          onClick={e => {
-            e.stopPropagation();
-          }}
-        >
-          <button onClick={toggleTable}>
-            {showTable ? "收起表格" : "展开表格"}
-          </button>
-          <ReactHTMLTableToExcel
-            id={`printExcelButton${index}`}
-            table={`productsOrderTable${index}`}
-            className="button"
-            filename="tablexls"
-            sheet="tablexls"
-            buttonText="Download as XLS"
-          />{" "}
-        </div>
+
       </div>
-      {showTable && (
-        <Table
-          id={`productsOrderTable${index}`}
-          ths={ths}
-          dataFormat={dataFormat}
-          data={orderProductArray}
-          sum={false}
-          striped={true}
-        />
-      )}
+      {orderProductArray.data.map((x) => {
+        return <div key={`order-product-row-divide-by-date-${orderProductArray.location}-${x.date}`} className={`sub-table`}>
+          <div className={`sub-table-section-header`}>
+            <span>{`${x.date} 共 ${getTotal(x.products)} 个菜`}</span>
+            <div
+              className={`control`}
+              onClick={e => {
+                e.stopPropagation();
+              }}
+            >
+              <button onClick={toggleTable}>
+                {showTable ? "收起表格" : "展开表格"}
+              </button>
+              <ReactHTMLTableToExcel
+                id={`printExcelButton${index}`}
+                table={`productsOrderTable${index}`}
+                className="button"
+                filename="tablexls"
+                sheet="tablexls"
+                buttonText="Download as XLS"
+              />
+            </div>
+          </div>
+          {showTable && (
+            <Table
+              id={`productsOrderTable${index}`}
+              ths={ths}
+              dataFormat={dataFormat}
+              data={x.products}
+              sum={false}
+              striped={true}
+            />
+          )}
+        </div>
+      })}
     </div>
   );
 };
@@ -56,7 +62,7 @@ const ths = [
   { value: "数量", type: "number" }
 ];
 const dataFormat = [
-  { value: "product_name", type: "text" },
+  { value: "name", type: "text" },
   { value: "quantity", type: "number" }
 ];
 
